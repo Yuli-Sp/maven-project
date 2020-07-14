18f8b2c490fa7381b5282f55752fc1ba475568c8
Yuliya Spiridonova <yuli@Yuliyas-MacBook-Pro.local>
2020-07-14 19:10:28 +0100

    test commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
 
 /**
 * @param someone the name of a person
-* @param greeting String
+* @param greeting Test
 */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
